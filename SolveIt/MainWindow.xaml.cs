d4a91634c41e6c508763266064f82b62d6b4aaea
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-10-15 14:09:51 +0100

    Made a simple GUI

--- SolveIt/MainWindow.xaml.cs
+++ SolveIt/MainWindow.xaml.cs
@@ -1,21 +1,12 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SolveIt
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        bool running = false;
         public MainWindow()
         {
             InitializeComponent();
