cc9048c269fce86a860a7489ae570097a5a6f7ee
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-10-16 19:33:49 +0100

    Formatting project structure

---
+++ SolveIt/View/UserControls/GraphDisplay.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace SolveIt.View.Graph
+{
+    /// <summary>
+    /// Interaction logic for GraphDisplay.xaml
+    /// </summary>
+    public partial class GraphDisplay : UserControl
+    {
+        public GraphDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateAnswer(int result)
+        {
+            AnswerText.Text = result.ToString();
+        }
+    }
+}
---
+++ SolveIt/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+
+namespace SolveIt.View.Windows;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        inputPanel.ResultCalculated += InputPanel_ResultCalculated;
+    }
+    private void InputPanel_ResultCalculated(object sender, int result)
+    {
+        graphDisplay.UpdateAnswer(result);
+    }
+}
